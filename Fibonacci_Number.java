0cccb6c4006ffc669e84a43b37d05c1daa6a4f4c
Anish <anishdharu05@gmail.com>
2026-03-02 00:47:13 +0530

    new files

--- Fibonacci_Number.java
+++ Fibonacci_Number.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Fibonacci_Number {
+public class Fibonacci_number{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int pre_pre = 0;
--- palindrone_number.java
+++ palindrone_number.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class palindrone_number {
+public class Palindrone_number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number to check palindrome: ");
