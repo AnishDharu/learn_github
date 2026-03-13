190082e131c590f44e3f39f2f126d0b133fe7bd9
Anish <anishdharu05@gmail.com>
2026-03-13 18:59:11 +0530

    Corrected Armstrong number program

--- Armstrong_number.java
+++ Armstrong_number.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Armstrong_number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
 
+        int k = String.valueOf(num).length();
         int original = num;
         int sum = 0;
 
-        while (num != 0) {
-            int digit = num % 10;
-            sum += digit * digit * digit;
+        while (num > 0) {
+            int ld = num % 10;      //lastdigit
+            sum += Math.pow(ld, k);
             num /= 10;
         }
 
